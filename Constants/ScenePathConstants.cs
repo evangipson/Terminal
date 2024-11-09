f2fee8e6f5e0c7485ee5984574d0a3ebf02ab78a
Evan Gipson <evangipson@gmail.com>
2024-11-09 15:52:14 -0600

    Document all constant files

--- Constants/ScenePathConstants.cs
+++ Constants/ScenePathConstants.cs
@@ -1,9 +1,23 @@
 namespace Terminal.Constants
 {
+    /// <summary>
+    /// A <see langword="static"/> collection of constant values that point to scenes in Godot.
+    /// </summary>
     public static class ScenePathConstants
     {
+        /// <summary>
+        /// The name of the introductory screen's scene in Godot.
+        /// </summary>
         public const string IntroScenePath = "IntroScreen";
+
+        /// <summary>
+        /// The name of the console screen's scene in Godot.
+        /// </summary>
         public const string ConsoleScenePath = "ConsoleScreen";
+
+        /// <summary>
+        /// The name of the welcome screen's scene in Godot.
+        /// </summary>
         public const string WelcomeScenePath = "WelcomeScreen";
     }
 }
--- Constants/ServicePathConstants.cs
+++ Constants/ServicePathConstants.cs
@@ -1,8 +1,21 @@
-﻿namespace Terminal.Constants
+﻿using Terminal.Navigators;
+using Terminal.Services;
+
+namespace Terminal.Constants
 {
+    /// <summary>
+    /// A <see langword="static"/> collection of constant values that point to singletons managed in Godot.
+    /// </summary>
     public static class ServicePathConstants
     {
+        /// <summary>
+        /// The <see cref="PersistService"/> singleton path in Godot.
+        /// </summary>
         public const string PersistServicePath = "/root/PersistService";
+
+        /// <summary>
+        /// The <see cref="ScreenNavigator"/> singleton path in Godot.
+        /// </summary>
         public const string ScreenNavigatorServicePath = "/root/ScreenNavigator";
     }
 }
